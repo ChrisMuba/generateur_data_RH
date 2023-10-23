13e26b9185f2a652a3519cd621f024068093b911
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-23 14:22:51 +0200

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -5,7 +5,8 @@ st.set_page_config(
     page_icon="🏠",
 )
 
-st.write("# Bienvenu dans Générateur de data RH! 👋🏿")
+st.write("# Bienvenu dans Générateur 
+de data RH! 👋🏿")
 
 st.sidebar.success("Select a demo above.")
 
