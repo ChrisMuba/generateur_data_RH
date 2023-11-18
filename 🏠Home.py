187896c737dfbb2d98575ef52d0208ec1800e79c
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-18 14:39:32 +0100

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -30,7 +30,7 @@ st.markdown(
     ### Vous voulez en savoir plus ?
      Le 🤖 Générateur de data RH comporte plusieurs pages, chacune axée sur la production de données pour une thématique RH différente :
     - Analyse du [turnover](https://data-rh.streamlit.app/%F0%9F%8F%83%F0%9F%8F%BC%E2%80%8D%E2%99%80%EF%B8%8FTurnover)
-    - Jump into our [documentation](https://docs.streamlit.io)
+    - Analyse de [l'attrition](https://data-rh.streamlit.app/Attrition)
     - Ask a question in our [community
         forums](https://discuss.streamlit.io)
 
