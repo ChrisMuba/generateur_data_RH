321049144010fef8ed7010fe2515d926c3206acf
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-18 16:50:08 +0100

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -15,7 +15,7 @@ st.markdown(
 """
 )
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Sélectionnez un item ci-dessus.")
 
 st.markdown(
     """
@@ -47,27 +47,6 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 
     
-"""
-)
-
-
-
-
-st.markdown(
-    """
-    Le générateur de données RH est une application gratuite spécialement conçue pour les projets d'études de données 
-    en **contrôle de gestion sociale, GPEC / GEPP et autres analyses** de données RH.  
-    **👈🏿 Sélectionnez un item dans la barre latérale** pour voir quelques exemples
-    de ce que le générateur de data RH peut produire !
-    ### Vous voulez en savoir plus ?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
 
