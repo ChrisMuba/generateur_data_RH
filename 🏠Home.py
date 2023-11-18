692107bb31d40d89c164a0cba1c0546b36895d3d
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-18 12:52:57 +0100

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -19,6 +19,37 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
+    Cette application a été créée pour fournir des exemples de données RH pouvant être utilisées à des fins d'analyse et de modélisation.📊
+
+    En tant qu'analyste data RH, avoir accès à des exemples de données réalistes est inestimable pour développer des compétences dans des domaines tels que la visualisation des données, l'analyse statistique et la modelisation. 
+    Cependant, les données RH réelles contiennent souvent des informations sensibles et ne peuvent pas être partagées publiquement. 
+    Cette application vise à résoudre ce problème en générant automatiquement des données RH fictives aux propriétés réalistes.💡
+    
+    
+    
+    
+    
+    Le générateur de données RH est une application gratuite spécialement conçue pour les projets d'études de données 
+    en **contrôle de gestion sociale, GPEC / GEPP et autres analyses** de données RH.  
+    **👈🏿 Sélectionnez un item dans la barre latérale** pour voir quelques exemples
+    de ce que le générateur de data RH peut produire !
+    ### Vous voulez en savoir plus ?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
+
+
+
+
+st.markdown(
+    """
     Le générateur de données RH est une application gratuite spécialement conçue pour les projets d'études de données 
     en **contrôle de gestion sociale, GPEC / GEPP et autres analyses** de données RH.  
     **👈🏿 Sélectionnez un item dans la barre latérale** pour voir quelques exemples
