22bd79b9c0e943c54f51152bc23957ce1f308bbe
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-18 18:25:51 +0100

    Update 4_🔄Attrition.py

--- pages/4_🔄Attrition.py
+++ pages/4_🔄Attrition.py
@@ -56,7 +56,7 @@ def generate_hr_data(num_entries):
 def main():
     st.title("Attrition Data Generator")
 
-    st.markdown("Analysez les facteurs contribuant à l'attrition et explorez les caractéristiques de ceux qui quittent l'entreprise. 🚪")
+    st.markdown("🔄 Analysez les facteurs contribuant à l'attrition et explorez les caractéristiques de ceux qui quittent l'entreprise.")
     
     num_entries = st.number_input("Entrer le nombre de données à générer (max 1000)", min_value=1, max_value=1000, value=50, step=1, format="%d")
     
