fda9e708ad80c2626ba97084e663c207c26122cc
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-18 13:09:37 +0100

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -22,7 +22,7 @@ st.markdown(
     Cette application a été créée pour fournir des exemples de données RH pouvant être utilisées à des fins d'analyse et de modélisation.📊
 
     En tant qu'analyste data RH, avoir accès à des exemples de données réalistes est inestimable pour développer des compétences dans des domaines tels que la visualisation des données, l'analyse statistique et la modelisation. 
-    Cependant, les données RH réelles contiennent souvent des informations sensibles et ne peuvent pas être partagées publiquement. 
+    Cependant, les données RH réelles contiennent souvent des informations sensibles et ne peuvent pas être partagées publiquement.🤐
     
     Cette application vise à résoudre ce problème en générant automatiquement des données RH fictives aux propriétés réalistes, et qui peuvent être utilisées pour des projets personnels, des tutoriels et autres analyses.💡
     
