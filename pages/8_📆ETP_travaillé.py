0e47c8dc048b6ef35ac7769659c07e82baf14eda
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-12-11 19:46:49 +0100

    Update 8_📆ETP_travaillé.py

--- pages/8_📆ETP_travaillé.py
+++ pages/8_📆ETP_travaillé.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import base64
 from faker import Faker
+from random import choices
 from random import choice
 
 # Instantiate a faker object
@@ -11,7 +12,13 @@ fake = Faker()
 def generate_row(emp_id):
     last_name = fake.last_name()
     first_name = fake.first_name()
-    num_hours_per_week = choice([35, 28, 24, 17.5])
+    #num_hours_per_week = choice([35, 28, 24, 17.5])
+
+    num_hours_per_week_weights = [0.827, 0.019, 0.036, 0.047, 0.03, 0.041]  # Weights for RH, Ventes, Marketing, etc... 
+    num_hours_per_week = choices([35, 32, 28, 24, 17.5, 15], weights=num_hours_per_week_weights, k=1)[0]
+
+
+    
     num_paid_hours_per_month = round(num_hours_per_week * 52 / 12, 2)
     fte = round(num_paid_hours_per_month / 151.67, 2)
     num_months_presence = choice(range(1, 13))
